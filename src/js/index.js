39d18bc4f3b66fb5a785e404e54fd542979a4fe2
Nelenik <100496821+Nelenik@users.noreply.github.com>
2023-08-21 01:14:05 +0300

    added images, changed concept

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,30 @@
 import '../index.html';
 import '../scss/index.scss';
+import Navigo from 'navigo';
+import { el, setChildren, mount } from 'redom';
+import { LS } from './js-parts/classes/Ls';
+
+const dataArr = [];
+
+const router = new Navigo('/');
+const container = el('div.container.page-container');
+mount(document.getElementById('page'), container);
+function createLink(isAdmin = false) {
+	return isAdmin
+		? el('a.nav-link.link-reset', { href: '/', 'data-navigo': '' }, 'Главная')
+		: el(
+				'a.nav-link.link-reset',
+				{ href: '/admin', 'data-navigo': '' },
+				'Панель администратора'
+		  );
+}
+
+router.on('/', () => {
+	setChildren(container, createLink());
+	console.log('hello');
+});
+router.on('/admin', () => {
+	setChildren(container, createLink(true));
+	console.log('admin page');
+});
+router.resolve();
---
+++ src/js/js-parts/modules/_form.js
@@ -0,0 +1,2 @@
+import { el, mount, setChildren } from 'redom';
+import { Select } from '../classes/Select';
